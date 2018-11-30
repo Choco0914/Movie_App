88bd364d041644b9d85d51d9824d5d03d0ce0215
Choco0914 <heojeongho1992@gmail.com>
2018-11-30 17:01:12 +0900

    Learn AJAX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count");
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header" />
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
