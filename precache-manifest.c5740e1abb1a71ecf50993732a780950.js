2d830e3c351446b485d2660e70c7cb3487724c0c
Choco0914 <heojeongho1992@gmail.com>
2018-12-01 13:59:35 +0900

    Updates

---
+++ precache-manifest.c5740e1abb1a71ecf50993732a780950.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae58143d64a8883e5f8b",
+    "url": "/Movie_App/static/js/runtime~main.ae58143d.js"
+  },
+  {
+    "revision": "74194459262f8c2cf34b",
+    "url": "/Movie_App/static/js/main.74194459.chunk.js"
+  },
+  {
+    "revision": "0bee8e5dcb3c5e319809",
+    "url": "/Movie_App/static/js/1.0bee8e5d.chunk.js"
+  },
+  {
+    "revision": "74194459262f8c2cf34b",
+    "url": "/Movie_App/static/css/main.781554cb.chunk.css"
+  },
+  {
+    "revision": "7c16c0e6e287a41b3ed7c4dc3494cc2c",
+    "url": "/Movie_App/index.html"
+  }
+];
